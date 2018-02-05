69aa72372e6be1baba5a6103d35c5512c3eb90e3
Sylmene <35269640+Sylmene@users.noreply.github.com>
2018-02-05 12:04:58 -0500

    Initial commit

---
+++ app/src/androidTest/java/com/example/luchi/simpletodo/MainActivityTest2.java
@@ -0,0 +1,81 @@
+package com.example.luchi.simpletodo;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest2 {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest2() {
+        ViewInteraction frameLayout = onView(
+                allOf(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class), isDisplayed()));
+        frameLayout.check(matches(isDisplayed()));
+
+        ViewInteraction frameLayout2 = onView(
+                allOf(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class), isDisplayed()));
+        frameLayout2.check(matches(isDisplayed()));
+
+        ViewInteraction frameLayout3 = onView(
+                allOf(withId(com.android.systemui.R.id.hided_by_cover_group2),
+                        childAtPosition(
+                                IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class),
+                                0),
+                        isDisplayed()));
+        frameLayout3.check(matches(isDisplayed()));
+
+        ViewInteraction frameLayout4 = onView(
+                allOf(withId(com.android.systemui.R.id.keyguard_effect_front),
+                        childAtPosition(
+                                allOf(withId(com.android.systemui.R.id.hided_by_cover_group2),
+                                        childAtPosition(
+                                                IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        frameLayout4.check(matches(isDisplayed()));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
